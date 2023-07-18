1c1e1b3701385df856bcd96741371c9bbca77ba9
Sezer Ozkan <sezer.ozzkan@gmail.com>
2023-07-18 16:51:14 +0200

    devam

--- src/test/java/kisiselKullanim/deneme2.java
+++ src/test/java/kisiselKullanim/deneme2.java
@@ -3,7 +3,7 @@ package kisiselKullanim;
 public class deneme2 {
     public static void main(String[] args) {
         System.out.println("sdfs");
-        System.out.println(";,nbg");
+        System.out.println("mlknxcn");
 
     }
 }
